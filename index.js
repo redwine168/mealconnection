ec5da131d7a94969b87ce775107be01ef634ca69
Jonathan Redwine <jredwine1@gmail.com>
2020-05-13 13:08:40 -0400

    Big update - new styles, landing page revamped

---
+++ index.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const url = require('url');
+
+// Additional setup and initialization
+const app = express();
+app.engine('.html', require('ejs').__express);
+app.set('view engine', 'html');
+const server = http.createServer(app);
+const port = process.env.PORT || 1337;
+const publicDirectoryPath = path.join(__dirname, 'public/')
+app.use(express.static(publicDirectoryPath))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set folders for html, css, and js files
+app.set('views', path.join(__dirname, '../public/views'))
+
+
+// GET for landing page
+app.get('/', function(req, res) {
+    console.log("Request for landing page");
+    res.sendFile(publicDirectoryPath + 'views/landingPage.html');
+})
+
+// GET for restaurant page
+app.get('/restaurant', function(req, res) {
+    console.log("Request for restaurant page");
+    res.sendFile(publicDirectoryPath + 'views/restaurant.html');
+})
+
+// GET for hospital page
+app.get('/hospital', function(req, res) {
+    console.log("Request for hospital page");
+    res.sendFile(publicDirectoryPath + 'views/hospital.html');
+})
+
+//This server is running through the port 1337
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
+});
