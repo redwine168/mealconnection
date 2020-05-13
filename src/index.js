20a901f098a05cae634821697432b6c6844e6be0
Jonathan Redwine <jredwine1@gmail.com>
2020-05-12 22:20:19 -0400

    First attempt for express() and having index.js point to html files

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
+const express = require('express')
 const http = require('http');
+const bodyParser = require('body-parser')
+const url = require('url')
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello gibby!");
-});
-
+// Additional setup and initialization
+const app = express()
+app.engine('.html', require('ejs').__express);
+app.set('view engine', 'html');
+const server = http.createServer(app)
 const port = process.env.PORT || 1337;
-server.listen(port);
+const publicDirectoryPath = path.join(__dirname, '../public/')
+app.use(express.static(publicDirectoryPath))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', path.join(__dirname, '../public/views'))
 
-console.log("Server running at http://localhost:%d", port);
+// Landing page for the app
+app.get('/', function (req, res) {
+    res.sendFile(publicDirectoryPath + 'views/landingPage.html');
+})
+
+//This server is running through the port 3000
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`)
+});
